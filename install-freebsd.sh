0005fa64a207e591dbd216ac9f8f061c1944a65e
Bykov Igor <igorb.dev@websm.io>
2017-03-07 00:03:53 +0300

    fix installer

--- install-freebsd.sh
+++ install-freebsd.sh
@@ -1,27 +1,31 @@
 #!/bin/sh 
 
 # requirements
-sudo pkg istall -y curl git nmp node xclip
+sudo pkg install -y curl git npm node xclip neovim python3 py35-setuptools35
 sudo npm install --global postcss-cli autoprefixer
+sudo easy_install-3.5 pip
+sudo pip install neovim
 
 # install plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # create folders
+mkdir -p ~/.config
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/nvim/colors
 
 # install config
-ln -s init.vim ~/.config/nvim/init.vim
-ln -s ginit.vim ~/.config/nvim/ginit.vim
+ln -sf `pwd`/init.vim ~/.config/nvim/init.vim
+ln -sf `pwd`/ginit.vim ~/.config/nvim/ginit.vim
 
 # install colorscheme
 pwd=`pwd`
 cd ..
 git clone https://github.com/joshdick/onedark.vim
-cd onedark/colors
-ln -s onedark.vim ~/.config/nvim/colors/onedark.vim
-cd $pwd
+cd onedark.vim/colors
+ln -sf `pwd`/onedark.vim ~/.config/nvim/colors/onedark.vim
+cd ~
 
 nvim -c PlugInstall
+nvim -c UpdateRemotePlugins
